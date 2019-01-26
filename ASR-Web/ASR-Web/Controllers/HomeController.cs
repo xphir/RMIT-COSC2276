a0faba68ac6d835467fb612ff533271d27ea49d3
Elliot Schot <s3530160@student.rmit.edu.au>
2019-01-26 14:13:30 +1100

    Added FAQ page

--- ASR-Web/ASR-Web/Controllers/HomeController.cs
+++ ASR-Web/ASR-Web/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace ASR_Web.Controllers
             return View();
         }
 
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
