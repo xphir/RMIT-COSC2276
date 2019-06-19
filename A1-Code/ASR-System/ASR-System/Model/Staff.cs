41a46c23afb90c9ef1068cc61c8b733c8584b483
Elliot Schot <Elliot.Schot@gmail.com>
2019-06-19 18:32:58 +1000

    Refactoring code layout + removing unused parts

--- A1-Code/ASR-System/ASR-System/Model/Staff.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ASR_System.Model
-{
-    public class Staff : User
-    {
-        public Staff(string userID, string name, string email) : base(userID, name, email) { }
-    }
-}
--- A1-Code/ASR-System/ASR-System/Model/Student.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ASR_System.Model
-{
-    public class Student : User
-    {
-        public Student(string userID, string name, string email) : base(userID, name, email) { }
-    }
-}
